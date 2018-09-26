37b37285c785bf7289aeaa15d8786339457f4dc0
JuviaYamired <juan.amesquita@ucsp.edu.pe>
2018-09-26 07:33:42 -0500

    added classplayer

--- Project-Vania/ClassPlayer.cpp
+++ Project-Vania/ClassPlayer.cpp
@@ -89,5 +89,6 @@ void ClassPlayer::walk()
 void ClassPlayer::jump(){
 	//position[0] = position[0] + velocity[1];
 	position[1] = position[1] + velocity[1];
+	
 	//position[1] = sqrt(2 * gravity * 4);
 }
