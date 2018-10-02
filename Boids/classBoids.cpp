563e1174460c351f9fc9b75dde0185814c511a3f
JuviaYamired <juan.amesquita@ucsp.edu.pe>
2018-10-01 19:30:10 -0500

    Boids

---
+++ Boids/classBoids.cpp
@@ -0,0 +1,19 @@
+#include "classBoids.h"
+
+classBoids::classBoids(){
+    
+}
+
+classBoids::classBoids(int mBoidsQuantity){
+    boidsQuantity = mBoidsQuantity;
+}
+
+void classBoids::initialize(){
+    for(int i=0; i < boidsQuantity; ++i){
+        
+    }
+}
+
+void classBoids::drawBoids(){
+
+}
---
+++ Boids/classBoids.h
@@ -0,0 +1,10 @@
+
+
+class classBoids{
+    public:
+        int boidsQuantity;
+        classBoids();
+        classBoids(int mBoidsQuantity);
+        void initialize();
+        void drawBoids();
+}
